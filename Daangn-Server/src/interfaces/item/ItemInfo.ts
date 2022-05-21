00986fdf76d5b6849069dec706318ee638198673
seohyun-106 <changseohyun@sookmyung.ac.kr>
2022-05-22 03:39:07 +0900

    add: Item Collection 추가

---
+++ Daangn-Server/src/interfaces/item/ItemInfo.ts
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+export interface ItemInfo {
+  like_id: mongoose.Types.ObjectId | string,
+  chat_id: mongoose.Types.ObjectId | string,
+  title: string,
+  location: string,
+  price: number,
+  image_list: string[],
+  content: string,
+  created_at: Date
+}
