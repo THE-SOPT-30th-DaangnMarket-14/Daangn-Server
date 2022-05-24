5a9a3d01611408150cc1003fd06c84d53dab594c
seohyun-106 <changseohyun@sookmyung.ac.kr>
2022-05-25 01:32:00 +0900

    chore: multer / AWS 시크릿 키 추가

--- Daangn-Server/src/config/index.ts
+++ Daangn-Server/src/config/index.ts
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import { access } from "fs";
 
 // Set the NODE_ENV to 'development' by default
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
@@ -20,4 +21,11 @@ export default {
    * MongoDB URI
    */
   mongoURI: process.env.MONGODB_URI as string,
+
+  /**
+   * AWS S3
+   */
+  s3AccessKey: process.env.S3_ACCESS_KEY as string,
+  s3SecretKey: process.env.S3_SECRET_KEY as string,
+  bucketName: process.env.BUCKET_NAME as string
 };
