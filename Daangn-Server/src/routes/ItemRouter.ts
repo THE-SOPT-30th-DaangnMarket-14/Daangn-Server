ac8ce28760c2f4fb100f9b439642dca00ecf02ce
jeonghoon <shb03323@naver.com>
2022-05-26 01:57:29 +0900

    [feat] add getItems in router

--- Daangn-Server/src/routes/ItemRouter.ts
+++ Daangn-Server/src/routes/ItemRouter.ts
@@ -5,5 +5,6 @@ import { upload } from "../middleware/Multer";
 const router: Router = Router();
 
 router.post("/", upload.array('images', 10), ItemController.createItem);
+router.get("/", ItemController.getItems);
 
 export default router;
