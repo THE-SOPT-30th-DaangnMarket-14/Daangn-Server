852fe59cb77961daa43608825223049f4739b968
seohyun-106 <changseohyun@sookmyung.ac.kr>
2022-05-27 01:48:29 +0900

    [feat] s3 연결

--- Daangn-Server/src/config/multerConfig.ts
+++ Daangn-Server/src/config/multerConfig.ts
@@ -1,16 +1,31 @@
 import { Request } from "express";
 import multer, { FileFilterCallback } from "multer";
+import storage from './s3Config';
+import multerS3 from 'multer-s3';
+import config from '.';
 
 type FileNameCallback = (error: Error | null, filename: string) => void;
 
+// export const multerConfig = {
+//   storage: multer.diskStorage({
+//     filename: function (
+//       req: Request,
+//       file: Express.Multer.File,
+//       cb: FileNameCallback
+//     ) {
+//       cb(null, file.originalname);
+//     },
+//   }),
+// };
+
 export const multerConfig = {
-  storage: multer.diskStorage({
-    filename: function (
-      req: Request,
-      file: Express.Multer.File,
-      cb: FileNameCallback
-    ) {
+  storage: multerS3 ({
+    s3: storage,
+    bucket: config.bucketName,
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: "public-read",
+    key: (req, file, cb) => {
       cb(null, file.originalname);
     },
-  }),
-};
+  })
+}
--- Daangn-Server/src/middleware/upload.ts
+++ Daangn-Server/src/middleware/upload.ts
@@ -1,6 +1,29 @@
 import multer from "multer";
+import storage from "../config/s3Config"
 import { multerConfig } from "../config/multerConfig";
+import multerS3 from 'multer-s3';
+import config from "../config";
+import aws from 'aws-sdk';
 
 const upload = multer(multerConfig);
+// const upload = multer();
+
+// const s3: aws.S3 = new aws.S3({
+//   accessKeyId: config.s3AccessKey,
+//   secretAccessKey: config.s3SecretKey,
+// });
+
+// const upload = multer({
+//   storage: multerS3 ({
+//     s3: s3,
+//     bucket: config.bucketName,
+//     contentType: multerS3.AUTO_CONTENT_TYPE,
+//     acl: "public-read",
+//     key: (req, file, cb) => {
+//       console.log(file);
+//       cb(null, file.originalname);
+//     }
+//   })
+// });
 
 export default upload;
