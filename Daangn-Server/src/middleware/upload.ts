ffbc0d0fbed24b18d5caf67f45c59ab8d6ae359a
seohyun-106 <changseohyun@sookmyung.ac.kr>
2022-05-27 22:13:56 +0900

    [chore] upload 로직 수정

--- Daangn-Server/src/middleware/upload.ts
+++ Daangn-Server/src/middleware/upload.ts
@@ -5,25 +5,24 @@ import multerS3 from 'multer-s3';
 import config from "../config";
 import aws from 'aws-sdk';
 
-// const upload = multer(multerConfig);
-// const upload = multer();
+const upload = multer(multerConfig);
 
-const s3: aws.S3 = new aws.S3({
-  accessKeyId: config.s3AccessKey,
-  secretAccessKey: config.s3SecretKey,
-});
+// const s3: aws.S3 = new aws.S3({
+//   accessKeyId: config.s3AccessKey,
+//   secretAccessKey: config.s3SecretKey,
+// });
 
-const upload = multer({
-  storage: multerS3 ({
-    s3: s3,
-    bucket: config.bucketName,
-    contentType: multerS3.AUTO_CONTENT_TYPE,
-    acl: "public-read",
-    key: (req, file, cb) => {
-      console.log(file);
-      cb(null, file.originalname);
-    }
-  })
-});
+// const upload = multer({
+//   storage: multerS3 ({
+//     s3: s3,
+//     bucket: config.bucketName,
+//     contentType: multerS3.AUTO_CONTENT_TYPE,
+//     acl: "public-read",
+//     key: (req, file, cb) => {
+//       console.log(file);
+//       cb(null, file.originalname);
+//     }
+//   })
+// });
 
 export default upload;
